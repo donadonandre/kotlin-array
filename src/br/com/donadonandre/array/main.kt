c1c2e1784005a6ba529bc51f9f7a47474173d1f9
André Donadon <andre.donadon@outlook.com>
2021-05-03 16:14:37 -0300

    AULA-3: Métodos buit in dos arrays

--- src/br/com/donadonandre/array/main.kt
+++ src/br/com/donadonandre/array/main.kt
@@ -1,30 +1,26 @@
 package br.com.donadonandre.array
 
 fun main() {
-    val serie: IntRange = 1.rangeTo(10)
-    for(s in serie) {
-        print("$s ")
-    }
+    val idades = intArrayOf(15, 16, 18, 19, 20, 21, 25, 30, 40, 50)
 
-    println()
+    val maiorIdade = idades.max() // Obsoleto
+    val menorIdade = idades.min() // Obsoleto
+    val mediaIdade = idades.average()
 
-    val numerosPares = 0..100 step 2 // 2.until(100)
-    for(numeroPar in numerosPares){
-        print("$numeroPar ")
-    }
+    println("Maior: $maiorIdade")
+    println("Menor: $menorIdade")
+    println("Média: $mediaIdade")
 
-    println()
+    val todosMaiores = idades.all { it >= 18 }
+    println("Todos Maiores? $todosMaiores")
 
-    val numerosParesReverso = 100 downTo 0 step 2
-    numerosParesReverso.forEach { print("$it ") }
+    val existeMaior = idades.any { it >= 18 }
+    println("Existe Maior? $existeMaior")
 
-    val intervalo = 1500.0..5000.0 // Vale para letras (alfabeto) também
-    val salario = 4000.0
-    if (salario in intervalo) {
-        println("O salário está dentro do intervalo")
-    }
-    else {
-        println("O salário NÃO está dentro do intervalo")
-    }
+    val maiores = idades.filter { it >= 18 }
+    println("Maiores alunos: $maiores" )
+
+    val busca = idades.find { it >= 18 }
+    println("Busca um de 18: $busca")
 }
 
