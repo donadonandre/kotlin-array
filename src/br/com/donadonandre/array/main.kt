32ee5688ab258988248d0d20e686d89df9d345de
André Donadon <andre.donadon@outlook.com>
2021-05-03 15:31:56 -0300

    Buscando a maior idade pelo Array com for each

--- src/br/com/donadonandre/array/main.kt
+++ src/br/com/donadonandre/array/main.kt
@@ -1,26 +1,19 @@
 package br.com.donadonandre.array
 
 fun main() {
-    val idade1 = 25
-    val idade2 = 19
-    val idade3 = 33
-    val idade4 = 20
+    val idades = IntArray(4)
+    idades[0] = 25
+    idades[1] = 19
+    idades[2] = 33
+    idades[3] = 22
 
-    val maiorIdade = if (idade1 > idade2 && idade1 > idade3 && idade1 > idade4) {
-        idade1
-    } else if (idade2 > idade3 && idade2 > idade3) {
-        idade2
-    } else if (idade3 > idade4) {
-        idade3
-    } else {
-        idade4
+    var maiorIdade = 0
+
+    for (idade in idades) {
+        if (idade > maiorIdade) {
+            maiorIdade = idade
+        }
     }
 
-    println(maiorIdade)
-
-    val idades = IntArray(4)
-    idades[0] = idade1
-    idades[1] = idade2
-    idades[2] = idade3
-    idades[3] = idade4
+    println("A maior idade é: $maiorIdade")
 }
