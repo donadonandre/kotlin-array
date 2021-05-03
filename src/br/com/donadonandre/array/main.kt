0862de472c0166ed35419030fe95561fb49d90d7
André Donadon <andre.donadon@outlook.com>
2021-05-03 18:19:07 -0300

    AULA:5 - Utilizando o groupBy junto com Elvis Operator

--- src/br/com/donadonandre/array/main.kt
+++ src/br/com/donadonandre/array/main.kt
@@ -1,10 +1,15 @@
 package br.com.donadonandre.array
 
 import br.com.donadonandre.list.Livro
-import br.com.donadonandre.testes.imprimeComMarcadores
 
 fun main() {
-    listaLivroComNulos.imprimeComMarcadores()
+    //listaLivroComNulos.imprimeComMarcadores()
+
+    listaLivroComNulos
+        .groupBy { it?.editora ?: "Editora desconhecida" }
+        .forEach { (editora, livros) ->
+            println("$editora: ${livros.joinToString { it?.titulo ?: "" }}")
+        }
 }
 
 val listaLivroComNulos: MutableList<Livro?> = mutableListOf(
@@ -14,7 +19,16 @@ val listaLivroComNulos: MutableList<Livro?> = mutableListOf(
         anoPublicacao = 2015,
         editora = "Editora A"
     ),
-    null,
+    Livro(
+        titulo = "Sagarana",
+        autor = "João Guimarães Rosa",
+        anoPublicacao = 1946
+    ),
+    Livro(
+        titulo = "12 Trabalhos de Hércules",
+        autor = "Monteiro Lobato",
+        anoPublicacao = 1905
+    ),
     Livro(
         titulo = "Memóricas Póstmas de Bras Cubas",
         autor = "Machado de Assis",
