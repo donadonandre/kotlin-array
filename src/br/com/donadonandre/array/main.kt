7358b1608bdfa26061dd16f82445dc697407a832
André Donadon <andre.donadon@outlook.com>
2021-05-03 17:45:13 -0300

    AULA-4: Criando uma lista de livros

--- src/br/com/donadonandre/array/main.kt
+++ src/br/com/donadonandre/array/main.kt
@@ -1,8 +1,45 @@
 package br.com.donadonandre.array
 
-import br.com.donadonandre.testes.testaAgregadoresEClassesExternas
+import br.com.donadonandre.list.Livro
 
 fun main() {
-    testaAgregadoresEClassesExternas()
+    val livro1 = Livro(
+        titulo = "Java Como Programar",
+        autor = "Deitel",
+        anoPublicacao = 2015,
+        editora = "Editora A"
+    )
+
+    val livro2 = Livro(
+        titulo = "Memóricas Póstmas de Bras Cubas",
+        autor = "Machado de Assis",
+        anoPublicacao = 1888,
+        editora = "Editora B"
+    )
+
+    val livro3 = Livro(
+        titulo = "Clean Code",
+        autor = "Uncle Bob",
+        anoPublicacao = 2009,
+        editora = "Editora C"
+    )
+
+    val livros: MutableList<Livro> = mutableListOf(livro1, livro2, livro3)
+
+    // Lista permite usar o método add, o array não
+
+    livros.add(
+        Livro(
+            titulo = "Sagarana",
+            autor = "João Guimarães Rosa",
+            anoPublicacao = 1946
+        )
+    )
+
+    println(livros)
+
+    livros.remove(livro2)
+
+    println("Depois de ter removido $livros")
 }
 
---
+++ src/br/com/donadonandre/list/Livro.kt
@@ -0,0 +1,10 @@
+package br.com.donadonandre.list
+
+data class Livro(
+    val titulo: String,
+    val autor: String,
+    val anoPublicacao: Long,
+    val editora: String? = null
+) {
+
+}
