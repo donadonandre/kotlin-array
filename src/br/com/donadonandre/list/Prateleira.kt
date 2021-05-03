0a05fcc12860cedff46d0779a38ae5ae68a192a4
André Donadon <andre.donadon@outlook.com>
2021-05-03 18:30:45 -0300

    AULA:5 - Listas NÃO mutaveis, método sortedBy

--- src/br/com/donadonandre/list/Prateleira.kt
+++ src/br/com/donadonandre/list/Prateleira.kt
@@ -2,15 +2,15 @@ package br.com.donadonandre.list
 
 class Prateleira(
     val genero: String,
-    val livros: MutableList<Livro?>
+    val livros: List<Livro?> // List é imutável
 ) {
-    fun organizarPorAutor(): MutableList<Livro>{
-        livros.sortedBy { it.autor }
-        return livros
+    fun organizarPorAutor(): List<Livro?> {
+        return livros.sortedBy { it?.autor }
+        // Aqui estava errado, quando era Mutable, o método certo era sortBy
     }
 
-    fun organizarPorAnoPublicacao(): MutableList<Livro> {
-        livros.sortedBy { it.anoPublicacao }
-        return livros
+    fun organizarPorAnoPublicacao(): List<Livro?> {
+        return livros.sortedBy { it?.anoPublicacao }
+        // Aqui estava errado, quando era Mutable, o método certo era sortBy
     }
 }
