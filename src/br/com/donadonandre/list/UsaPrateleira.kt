ceebb38bac1a09c5b9f76ae2ad5fd9489a6fdbbb
André Donadon <andre.donadon@outlook.com>
2021-05-04 15:58:22 -0300

    C6-A3: Funções do Map

--- src/br/com/donadonandre/list/UsaPrateleira.kt
+++ src/br/com/donadonandre/list/UsaPrateleira.kt
@@ -1,7 +1,11 @@
 package br.com.donadonandre.list
 
 fun main() {
-    val pedidos = mapOf<Int, Double>(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+    val pedidos = mutableMapOf<Int, Double>(
+        Pair(1, 20.0),
+        Pair(2, 34.0),
+        3 to 50.0
+    )
     // 3 to 50.0 tem problemas de performance
     println(pedidos)
 
@@ -10,9 +14,28 @@ fun main() {
         println("pedido $it")
     }
 
-    for (pedido in pedidos) { // pedido: Map.Entry
-        println("Número do pedido: ${pedido.key}")
-        println("Valor do pedido: ${pedido.value}")
+    for (p in pedidos) { // pedido: Map.Entry
+        println("Número do pedido: ${p.key}")
+        println("Valor do pedido: ${p.value}")
     }
+
+    // Adicionando...
+    pedidos[4] = 70.0
+    println(pedidos)
+
+    pedidos.put(5, 80.0) // Convencional é a versão de cima
+    println(pedidos)
+
+    // Atualizando...
+    pedidos[1] = 100.0
+    println(pedidos)
+
+    // Adicionando caso não exista (não atualiza)
+    pedidos.putIfAbsent(2, 600.0)
+    println(pedidos)
+
+    // Removendo
+    pedidos.remove(3) // Pode colocar o value junto para garantir melhor
+    println(pedidos)
 }
 
