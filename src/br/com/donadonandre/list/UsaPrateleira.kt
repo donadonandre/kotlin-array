085c8e9317a1a0f1d218a05c15f0fb22a6f103df
André Donadon <andre.donadon@outlook.com>
2021-05-04 16:44:03 -0300

    C6-A5: Usando o groupBy

--- src/br/com/donadonandre/list/UsaPrateleira.kt
+++ src/br/com/donadonandre/list/UsaPrateleira.kt
@@ -19,6 +19,38 @@ fun main() {
     }
     println(pedidosFreteGratis)
     println(pedidosFreteGratis[Pedido(numero=2, valor=30.0)])
+
+    // Agrupar mais de um valor dentro de uma chave
+//    val mapa: Map<Boolean, Pedido> = pedidos.associateBy { pedido -> pedido.valor > 50.0 }
+//    println(mapa)
+
+    val pedidosFreteGratisAgrupados: Map<Boolean, List<Pedido>> = pedidos.groupBy { pedido: Pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratisAgrupados)
+
+    val nomes = listOf(
+        "André",
+        "Pedro",
+        "João",
+        "Ana",
+        "Diego",
+        "Guilherme",
+        "Bruna",
+        "Barbara"
+    )
+
+    val agenda: Map<Char, List<String>> = nomes.groupBy { nome ->
+        nome.first()
+    }
+    println(agenda)
+    println(agenda['A'])
+
+    //  Também podemos usar o groupingBy
+    val pedidosAgrupados: Grouping<Pedido, Boolean> = pedidos.groupingBy { pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosAgrupados.eachCount())
 }
 
 data class Pedido(val numero: Int, val valor: Double)
