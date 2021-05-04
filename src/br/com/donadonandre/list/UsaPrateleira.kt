34eb4b886abaa88225848b17c760825f40de169e
André Donadon <andre.donadon@outlook.com>
2021-05-04 15:37:43 -0300

    C6-A3: Trabalhando com Sets

--- src/br/com/donadonandre/list/UsaPrateleira.kt
+++ src/br/com/donadonandre/list/UsaPrateleira.kt
@@ -1,13 +1,28 @@
 package br.com.donadonandre.list
 
 fun main() {
-    val assistiramCursoAndroid: List<String> = listOf("André", "Alex", "Jederson")
+    val assistiramCursoAndroid: Set<String> = setOf("André", "Alex", "Jederson")
     val assistiramCursoKotlin: List<String> = listOf("André", "Alex", "Jederson", "João", "Maria")
-    val assistiramAmbos = mutableListOf<String>()
 
-    assistiramAmbos.addAll(assistiramCursoKotlin)
+    val assistiramAmbos: MutableSet<String> = mutableSetOf()
     assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Ana")
+    assistiramAmbos.add("Ana")
+
+    println(assistiramAmbos)
+
+
+    /*
+//    val assistiramAmbos = mutableListOf<String>()
+    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
+
+    //assistiramAmbos.addAll(assistiramCursoKotlin)
+    //assistiramAmbos.addAll(assistiramCursoAndroid)
 
     println(assistiramAmbos.distinct())
     // [André, Alex, Jederson, João, Maria]
+//    println(assistiramAmbos[0]) // Esse código não funciona para set
+    */
+
 }
